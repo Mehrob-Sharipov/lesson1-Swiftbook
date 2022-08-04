7342c57e45b7c7b1505729738d645b255d8a570b
Mehrob <shmehrob16@gmail.com>
2022-08-04 06:58:23 +0500

    update

--- lesson1 Swiftbook/ViewController.swift
+++ lesson1 Swiftbook/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  
+    @IBOutlet weak var lable: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        lable.isHidden = true
+        button.layer.cornerRadius = 10
     }
 
+    @IBAction func buttonAction(_ sender: UIButton) {
+        
+        if lable.isHidden == false {
+        button.backgroundColor = .yellow
+            button.setTitle("Show Text!", for: .normal)
+            lable.isHidden = true
+        } else if lable.isHidden == true {
+            button.setTitle("Hide Text!", for: .normal)
 
+            button.backgroundColor = .green
+            lable.textColor = .green
+            lable.isHidden = false
+           
+        }
+    }
+    
 }
 
